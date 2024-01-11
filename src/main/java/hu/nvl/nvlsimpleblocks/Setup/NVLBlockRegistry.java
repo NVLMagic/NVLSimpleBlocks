3d8e43069473a0a91660873196e4af57105ce448
NVLMagic <nvl@nvl.hu>
2024-01-11 16:37:03 +0100

    v4 - Add dye blocks

--- src/main/java/hu/nvl/nvlsimpleblocks/Setup/NVLBlockRegistry.java
+++ src/main/java/hu/nvl/nvlsimpleblocks/Setup/NVLBlockRegistry.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 public class NVLBlockRegistry implements NVLInterfaceLogger {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, NVLSimpleBlocks.MODID);
 	private static final ArrayList<RegistryObject<Block>> colored = new ArrayList<>();
-	private static int registerColoredBlocks(String name, String code, Block source, boolean color) {
+	private static int registerColoredBlocks(String name, String code, Block source, boolean color, boolean dodye) {
 		RegistryObject<Block> stair = BLOCKS.register("nvl_block_"+code+"t"+name, () -> new StairBlock(source::defaultBlockState,BlockBehaviour.Properties.copy(source)));
 		registerBlockItem("nvl_block_"+code+"t"+name, stair);
 		if (color) colored.add(stair);
@@ -25,42 +25,48 @@ public class NVLBlockRegistry implements NVLInterfaceLogger {
 		RegistryObject<Block> wall = BLOCKS.register("nvl_block_"+code+"w"+name, () -> new WallBlock(BlockBehaviour.Properties.copy(source).forceSolidOn()));
 		registerBlockItem("nvl_block_"+code+"w"+name, wall);
 		if (color) colored.add(wall);
+		if (dodye) {
+			// And now the dye block
+			RegistryObject<Block> dye = BLOCKS.register("nvl_block_dye" + name, () -> new Block(BlockBehaviour.Properties.copy(Blocks.CLAY)));
+			registerBlockItem("nvl_block_dye" + name, dye);
+			if (color) colored.add(dye);
+		}
 		return 0;
 	}
-	public static final int NVL_Block_TS = registerColoredBlocks("","ts",Blocks.TERRACOTTA,false);
-	public static final int NVL_Block_TB_Black = registerColoredBlocks("_black","ts",Blocks.BLACK_TERRACOTTA, true);
-	public static final int NVL_Block_TB_White = registerColoredBlocks("_white","ts",Blocks.WHITE_TERRACOTTA, true);
-	public static final int NVL_Block_TB_Brown = registerColoredBlocks("_brown","ts",Blocks.BROWN_TERRACOTTA, true);
-	public static final int NVL_Block_TB_LGray = registerColoredBlocks("_light_gray","ts",Blocks.LIGHT_GRAY_TERRACOTTA, true);
-	public static final int NVL_Block_TB_Gray = registerColoredBlocks("_gray","ts",Blocks.GRAY_TERRACOTTA, true);
-	public static final int NVL_Block_TB_Red = registerColoredBlocks("_red","ts",Blocks.RED_TERRACOTTA, true);
-	public static final int NVL_Block_TB_Orange = registerColoredBlocks("_orange","ts",Blocks.ORANGE_TERRACOTTA,true);
-	public static final int NVL_Block_TB_Yellow = registerColoredBlocks("_yellow","ts",Blocks.YELLOW_TERRACOTTA,true);
-	public static final int NVL_Block_TB_Lime = registerColoredBlocks("_lime","ts",Blocks.LIME_TERRACOTTA,true);
-	public static final int NVL_Block_TB_Green = registerColoredBlocks("_green","ts",Blocks.GREEN_TERRACOTTA,true);
-	public static final int NVL_Block_TB_Cyan = registerColoredBlocks("_cyan","ts",Blocks.CYAN_TERRACOTTA,true);
-	public static final int NVL_Block_TB_LBlue = registerColoredBlocks("_light_blue","ts",Blocks.LIGHT_BLUE_TERRACOTTA,true);
-	public static final int NVL_Block_TB_Blue = registerColoredBlocks("_blue","ts",Blocks.BLUE_TERRACOTTA,true);
-	public static final int NVL_Block_TB_Purple = registerColoredBlocks("_purple","ts",Blocks.PURPLE_TERRACOTTA,true);
-	public static final int NVL_Block_TB_Magenta = registerColoredBlocks("_magenta","ts",Blocks.MAGENTA_TERRACOTTA,true);
-	public static final int NVL_Block_TB_Pink = registerColoredBlocks("_pink","ts",Blocks.PINK_TERRACOTTA,true);
+	public static final int NVL_Block_TS = registerColoredBlocks("","ts",Blocks.TERRACOTTA,false,false);
+	public static final int NVL_Block_TB_Black = registerColoredBlocks("_black","ts",Blocks.BLACK_TERRACOTTA, true,true);
+	public static final int NVL_Block_TB_White = registerColoredBlocks("_white","ts",Blocks.WHITE_TERRACOTTA, true,true);
+	public static final int NVL_Block_TB_Brown = registerColoredBlocks("_brown","ts",Blocks.BROWN_TERRACOTTA, true,true);
+	public static final int NVL_Block_TB_LGray = registerColoredBlocks("_light_gray","ts",Blocks.LIGHT_GRAY_TERRACOTTA, true,true);
+	public static final int NVL_Block_TB_Gray = registerColoredBlocks("_gray","ts",Blocks.GRAY_TERRACOTTA, true,true);
+	public static final int NVL_Block_TB_Red = registerColoredBlocks("_red","ts",Blocks.RED_TERRACOTTA, true,true);
+	public static final int NVL_Block_TB_Orange = registerColoredBlocks("_orange","ts",Blocks.ORANGE_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_Yellow = registerColoredBlocks("_yellow","ts",Blocks.YELLOW_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_Lime = registerColoredBlocks("_lime","ts",Blocks.LIME_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_Green = registerColoredBlocks("_green","ts",Blocks.GREEN_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_Cyan = registerColoredBlocks("_cyan","ts",Blocks.CYAN_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_LBlue = registerColoredBlocks("_light_blue","ts",Blocks.LIGHT_BLUE_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_Blue = registerColoredBlocks("_blue","ts",Blocks.BLUE_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_Purple = registerColoredBlocks("_purple","ts",Blocks.PURPLE_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_Magenta = registerColoredBlocks("_magenta","ts",Blocks.MAGENTA_TERRACOTTA,true,true);
+	public static final int NVL_Block_TB_Pink = registerColoredBlocks("_pink","ts",Blocks.PINK_TERRACOTTA,true,true);
 	// ---------------- Concrete ----------------
-	public static final int NVL_Block_CB_Black = registerColoredBlocks("_black","cs",Blocks.BLACK_CONCRETE, true);
-	public static final int NVL_Block_CB_White = registerColoredBlocks("_white","cs",Blocks.WHITE_CONCRETE, true);
-	public static final int NVL_Block_CB_Brown = registerColoredBlocks("_brown","cs",Blocks.BROWN_CONCRETE, true);
-	public static final int NVL_Block_CB_LGray = registerColoredBlocks("_light_gray","cs",Blocks.LIGHT_GRAY_CONCRETE, true);
-	public static final int NVL_Block_CB_Gray = registerColoredBlocks("_gray","cs",Blocks.GRAY_CONCRETE, true);
-	public static final int NVL_Block_CB_Red = registerColoredBlocks("_red","cs",Blocks.RED_CONCRETE, true);
-	public static final int NVL_Block_CB_Orange = registerColoredBlocks("_orange","cs",Blocks.ORANGE_CONCRETE,true);
-	public static final int NVL_Block_CB_Yellow = registerColoredBlocks("_yellow","cs",Blocks.YELLOW_CONCRETE,true);
-	public static final int NVL_Block_CB_Lime = registerColoredBlocks("_lime","cs",Blocks.LIME_CONCRETE,true);
-	public static final int NVL_Block_CB_Green = registerColoredBlocks("_green","cs",Blocks.GREEN_CONCRETE,true);
-	public static final int NVL_Block_CB_Cyan = registerColoredBlocks("_cyan","cs",Blocks.CYAN_CONCRETE,true);
-	public static final int NVL_Block_CB_LBlue = registerColoredBlocks("_light_blue","cs",Blocks.LIGHT_BLUE_CONCRETE,true);
-	public static final int NVL_Block_CB_Blue = registerColoredBlocks("_blue","cs",Blocks.BLUE_CONCRETE,true);
-	public static final int NVL_Block_CB_Purple = registerColoredBlocks("_purple","cs",Blocks.PURPLE_CONCRETE,true);
-	public static final int NVL_Block_CB_Magenta = registerColoredBlocks("_magenta","cs",Blocks.MAGENTA_CONCRETE,true);
-	public static final int NVL_Block_CB_Pink = registerColoredBlocks("_pink","cs",Blocks.PINK_CONCRETE,true);
+	public static final int NVL_Block_CB_Black = registerColoredBlocks("_black","cs",Blocks.BLACK_CONCRETE, true,false);
+	public static final int NVL_Block_CB_White = registerColoredBlocks("_white","cs",Blocks.WHITE_CONCRETE, true,false);
+	public static final int NVL_Block_CB_Brown = registerColoredBlocks("_brown","cs",Blocks.BROWN_CONCRETE, true,false);
+	public static final int NVL_Block_CB_LGray = registerColoredBlocks("_light_gray","cs",Blocks.LIGHT_GRAY_CONCRETE, true,false);
+	public static final int NVL_Block_CB_Gray = registerColoredBlocks("_gray","cs",Blocks.GRAY_CONCRETE, true,false);
+	public static final int NVL_Block_CB_Red = registerColoredBlocks("_red","cs",Blocks.RED_CONCRETE, true,false);
+	public static final int NVL_Block_CB_Orange = registerColoredBlocks("_orange","cs",Blocks.ORANGE_CONCRETE,true,false);
+	public static final int NVL_Block_CB_Yellow = registerColoredBlocks("_yellow","cs",Blocks.YELLOW_CONCRETE,true,false);
+	public static final int NVL_Block_CB_Lime = registerColoredBlocks("_lime","cs",Blocks.LIME_CONCRETE,true,false);
+	public static final int NVL_Block_CB_Green = registerColoredBlocks("_green","cs",Blocks.GREEN_CONCRETE,true,false);
+	public static final int NVL_Block_CB_Cyan = registerColoredBlocks("_cyan","cs",Blocks.CYAN_CONCRETE,true,false);
+	public static final int NVL_Block_CB_LBlue = registerColoredBlocks("_light_blue","cs",Blocks.LIGHT_BLUE_CONCRETE,true,false);
+	public static final int NVL_Block_CB_Blue = registerColoredBlocks("_blue","cs",Blocks.BLUE_CONCRETE,true,false);
+	public static final int NVL_Block_CB_Purple = registerColoredBlocks("_purple","cs",Blocks.PURPLE_CONCRETE,true,false);
+	public static final int NVL_Block_CB_Magenta = registerColoredBlocks("_magenta","cs",Blocks.MAGENTA_CONCRETE,true,false);
+	public static final int NVL_Block_CB_Pink = registerColoredBlocks("_pink","cs",Blocks.PINK_CONCRETE,true,false);
 
 	private static void registerBlockItem(String name, RegistryObject<Block> block) {
 		NVLItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
